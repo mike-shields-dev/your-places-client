58be537718210efe780aa3b0da1d456a3bce521a
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-17 13:13:11 +0000

    feat: Improve USERS data in Users

--- src/user/pages/Users/index.jsx
+++ src/user/pages/Users/index.jsx
@@ -4,28 +4,28 @@ import { UsersList } from '../../components';
 const Users = () => {
     const USERS = [
         {
-            id: "user1",
-            name: 'User Name 1',
+            id: "user0",
+            name: 'Sarah Brightman',
             places: [2],
-            image: 'https://i.pravatar.cc/200'
+            image: 'https://i.pravatar.cc/200?img=0'
         },
         {
             id: "user2",
-            name: 'User Name 2',
+            name: 'Louise Makinson',
             places: [5],
-            image: 'https://i.pravatar.cc/200'
+            image: 'https://i.pravatar.cc/200?img=1'
         },
         {
             id: "user3",
-            name: 'User Name 3',
+            name: 'James Brampton',
             places: [4],
-            image: 'https://i.pravatar.cc/200'
+            image: 'https://i.pravatar.cc/200?img=4'
         },
         {
             id: "user4",
-            name: 'User Name 4',
+            name: 'Scott Davies',
             places: [9],
-            image: 'https://i.pravatar.cc/200'
+            image: 'https://i.pravatar.cc/200?img=3'
         },
     ];
 
