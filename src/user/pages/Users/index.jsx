c1cfccfc04e68ec3e59bb8cab6ba194fa69d9cfd
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-24 15:21:45 +0000

    feat: Users list requests data from api

--- src/user/pages/Users/index.jsx
+++ src/user/pages/Users/index.jsx
@@ -1,36 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { UsersList } from '../../components';
+import LoadingSpinner from '../../../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from '../../../shared/components/UIElements/ErrorModal';
+import { useHttpClient } from '../../../shared/hooks';
+
 
 const Users = () => {
-    const USERS = [
-        {
-            id: 'user0',
-            name: 'Sarah Brightman',
-            places: [2],
-            image: 'https://i.pravatar.cc/200?img=0'
-        },
-        {
-            id: 'user1',
-            name: 'Louise Makinson',
-            places: [5],
-            image: 'https://i.pravatar.cc/200?img=1'
-        },
-        {
-            id: 'user2',
-            name: 'James Brampton',
-            places: [4],
-            image: 'https://i.pravatar.cc/200?img=4'
-        },
-        {
-            id: 'user3',
-            name: 'Scott Davies',
-            places: [9],
-            image: 'https://i.pravatar.cc/200?img=3'
-        },
-    ];
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const [uncaughtError, setUncaughtError] = useState();
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        try {
+         (async () => {
+            const data = 
+                await sendRequest(
+                    'http://localhost:5000/api/users'
+                );
+
+            setUsers(data.users);
+         })();
+
+        } catch (error) {
+            setUncaughtError(error.message)
+        }
+
+    }, [sendRequest]); 
+
+    const clearErrorHandler = () => clearError(); 
 
     return (
-        <UsersList items={USERS} />
+        <>
+            <ErrorModal error={error || uncaughtError} onClear={clearErrorHandler} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && users && <UsersList items={users} />}
+        </>
     );
 }
 
