b0675fdf659d02a7d9b27e354f99bcc6f189f716
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-24 21:38:11 +0000

    feat: Submitting NewPlace form adds a new place to the DB

--- src/places/pages/NewPlace/index.jsx
+++ src/places/pages/NewPlace/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import AuthContext from '../../../shared/context/auth-context';
 
 import Input from '../../../shared/components/FormElements/Input';
 import Button from '../../../shared/components/FormElements/Button';
+import ErrorModal from '../../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../../shared/components/UIElements/LoadingSpinner';
 
 import useForm from '../../../shared/hooks/useForm';
+import useHttpClient from '../../../shared/hooks/useHttpClient';
 import deriveFormState from '../../../shared/components/util/deriveFormState';
 
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../../shared/components/util/validators';
@@ -44,32 +50,55 @@ const initFormState = deriveFormState(formInputs);
 
 const NewPlace = () => {
     const [formState, handleInput] = useForm(initFormState, true);
+    const { sendRequest, isLoading, error, clearError } = useHttpClient();
+    const { userId } = useContext(AuthContext);
+    const history = useHistory();
 
-    const handleFormSubmit = event => {
+    const handleFormSubmit = async event => {
         event.preventDefault();
-        console.log(formState.inputs);
+        
+        try {
+            await sendRequest(
+                'http://localhost:5000/api/places',
+                'POST',
+                JSON.stringify({
+                    title: formState.inputs.title.value,
+                    description: formState.inputs.description.value,
+                    address: formState.inputs.address.value,
+                    creator: userId, 
+                }),
+                { 'Content-Type': 'application/json' }
+            )
+            history.push('/')
+        } catch (error) {
+            console.log(error.message);
+        }
     };
 
     return (
-        <form
-            className='place-form'
-            onSubmit={handleFormSubmit}
-        >
-            {formInputs.map(({
-                id,
-                element,
-                errorText,
-                label,
-                validators
-            }) =>
-                <Input
-                    key={`new-place-form$-${id}`}
-                    onInput={handleInput}
-                    {...{ id, element, label, validators, errorText }}
-                />
-            )}
-            <Button disabled={!formState.isValid}>Add Place</Button>
-        </form>
+        <>
+            <ErrorModal error={error} onClear={clearError} />
+            <form
+                className='place-form'
+                onSubmit={handleFormSubmit}
+            >
+            {isLoading && <LoadingSpinner  asOverlay />}
+                {formInputs.map(({
+                    id,
+                    element,
+                    errorText,
+                    label,
+                    validators
+                }) =>
+                    <Input
+                        key={`new-place-form$-${id}`}
+                        onInput={handleInput}
+                        {...{ id, element, label, validators, errorText }}
+                    />
+                )}
+                <Button disabled={!formState.isValid}>Add Place</Button>
+            </form>
+        </>
     );
 }
 
