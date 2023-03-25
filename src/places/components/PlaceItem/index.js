2d4bf13ab574540fdb430b842cf0b68243a00ab8
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-25 13:45:10 +0000

    feat: A logged in user can update their own places

--- src/places/components/PlaceItem/index.js
+++ src/places/components/PlaceItem/index.js
@@ -1,17 +1,24 @@
 import React, { Fragment, useState, useContext } from 'react'
 import AuthContext from '../../../shared/context/auth-context';
+import { useHistory } from 'react-router-dom';
 
 import Card from '../../../shared/components/UIElements/Card';
 import Button from '../../../shared/components/FormElements/Button';
 import Modal from '../../../shared/components/UIElements/Modal';
 import Map from '../../../shared/components/UIElements/Map';
+import ErrorModal from '../../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../../shared/components/UIElements/LoadingSpinner';
+
+import useHttpClient from '../../../shared/hooks/useHttpClient';
 
 import './styles.css';
 
 const PlaceItem = props => {
     const { isLoggedIn } = useContext(AuthContext);
+    const { sendRequest, isLoading, error, clearError } = useHttpClient();
     const [showMap, setShowMap] = useState(false);
     const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const history = useHistory();
 
     const handleOpenMap = () => setShowMap(true);
     const handleCloseMap = () => setShowMap(false);
@@ -19,13 +26,27 @@ const PlaceItem = props => {
     const handleShowDeleteModal = () => setShowDeleteModal(true);
     const handleHideDeleteModal = () => setShowDeleteModal(false);
 
-    const handleDeletePlace = () => {
-        console.log("deleted")
-        setShowDeleteModal(false);
-    }
+    const handleDeletePlace = async () => {
+        (async () => {
+            try {
+                await sendRequest(
+                    `http://localhost:5000/api/places/${props.id}`,
+                    'DELETE',
+                )
+                
+            } catch (error) {
+
+            }
+
+            setShowDeleteModal(false);
+            history.go(0);
+        })()
+    }        
 
     return (
         <Fragment>
+            <ErrorModal error={error} clear={clearError} />
+            {isLoading && <LoadingSpinner />}
             <Modal
                 show={showMap}
                 onCancel={handleCloseMap}
--- src/places/pages/UpdatePlace/index.jsx
+++ src/places/pages/UpdatePlace/index.jsx
@@ -1,149 +1,125 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react'
+import AuthContext from '../../../shared/context/auth-context';
+import { useParams, useHistory } from 'react-router-dom';
 
 import Button from '../../../shared/components/FormElements/Button';
 import Card from '../../../shared/components/UIElements/Card';
 import Input from '../../../shared/components/FormElements/Input';
+import LoadingSpinner from '../../../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from '../../../shared/components/UIElements/ErrorModal';
 
 import useForm from '../../../shared/hooks/useForm';
+import useHttpClient from '../../../shared/hooks/useHttpClient';
 
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../../shared/components/util/validators';
 
 import '../styles.css';
 
-const PLACES = [
-    {
-        id: 'place0',
-        title: 'Empire State Building',
-        description: 'One of the most famous buildings in the world.',
-        imageUrl: 'https://marvel-b1-cdn.bc0a.com/f00000000179470/www.esbnyc.com/sites/default/files/styles/small_feature/public/2019-10/home_banner-min.jpg?itok=uZt-03Vw',
-        address: '20 W 34th St., New York, NY 10001, United States',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878531,
-        },
-        creator: 'user1',
-    },
-    {
-        id: 'place1',
-        title: 'Machu Picchu',
-        description: 'Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley. Built in the 15th century and later abandoned, it’s renowned for its sophisticated dry-stone walls that fuse huge blocks without the use of mortar, intriguing buildings that play on astronomical alignments and panoramic views. Its exact former use remains a mystery.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Before_Machu_Picchu.jpg/1200px-Before_Machu_Picchu.jpg',
-        address: 'Peru',
-        location: {
-            lat: -13.2263308,
-            lng: -72.4995102,
-        },
-        creator: 'user1',
-    },
-];
-
-const formInputs = [
-    {
-        id: 'title',
-        label: 'Title',
-        value: '',
-        isValid: false,
-        element: 'input',
-        errorText: 'Please enter a valid title.',
-        minLength: 5,
-        validators: [VALIDATOR_REQUIRE()]
-    },
-    {
-        id: 'description',
-        label: 'Description',
-        value: '',
-        isValid: false,
-        element: 'textarea',
-        errorText: 'Please enter a valid description (5 characters minimum).',
-        validators: [VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]
-    },
-];
-
-const initFormState = formInputs.reduce((initFormState, formInput) => {
-    return {
-        ...initFormState,
-        [formInput.id]: {
-            value: formInput.value,
-            isValid: formInput.isValid,
-        }
-    }
-}, {});
-
 const UpdatePlace = () => {
-    const [formState, handleInput, setFormState] = useForm(initFormState, false);
-
+    const auth = useContext(AuthContext);
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const [loadedPlace, setLoadedPlace] = useState();
     const placeId = useParams().placeId;
-
-    const foundPlace = PLACES.find(place => place.id === placeId);
-
+    const history = useHistory();
+  
+    const [formData, inputHandler, setFormData] = useForm();
+  
     useEffect(() => {
-        if (foundPlace) {
+          (async () => {
+            try {
+                const responseData = await sendRequest(
+                  `http://localhost:5000/api/places/${placeId}`
+                );
+                setLoadedPlace(responseData.place);
+                setFormData(
+                  {
+                    title: {
+                      value: responseData.place.title,
+                      isValid: true
+                    },
+                    description: {
+                      value: responseData.place.description,
+                      isValid: true
+                    }
+                  },
+                  true
+                );
+              } catch (err) {}
+            })();
 
-            const { title, description } = foundPlace;
-
-            setFormState({
-                title: {
-                    value: title,
-                    isValid: true,
-                },
-                description: {
-                    value: description,
-                    isValid: true,
-                }
-            }, true);
-        }
-
-    }, [foundPlace, setFormState]);
-
-    if (!foundPlace) {
-        return (
-            <div className='center'>
-                <Card>
-                    <h2>No place found. </h2>
-                </Card>
-            </div>
+    }, [sendRequest, placeId, setFormData]);
+  
+    const placeUpdateSubmitHandler = async event => {
+      event.preventDefault();
+      try {
+        await sendRequest(
+          `http://localhost:5000/api/places/${placeId}`,
+          'PATCH',
+          JSON.stringify({
+            title: formData.inputs.title.value,
+            description: formData.inputs.description.value
+          }),
+          {
+            'Content-Type': 'application/json'
+          }
         );
-    }
-
-    const handleFormSubmit = event => {
-        event.preventDefault();
-        console.log(formState.inputs);
+        history.push('/' + auth.userId + '/places');
+      } catch (err) {}
     };
+  
+    useEffect(() => {}, [formData]);
+    
+    if (isLoading) {
+      return (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      );
+    }
+  
+    if (!loadedPlace && !error && !isLoading) {
+      return (
+        <div className="center">
+          <Card>
+            <h2>Could not find place!</h2>
+          </Card>
+        </div>
+      );
+    }
 
     return (
-        <form
-            className='place-form'
-            onSubmit={handleFormSubmit}
-        >
-            {formInputs.map(({
-                id,
-                element,
-                errorText,
-                label,
-                validators,
-                isValid,
-            }) =>
-                <Input
-                    key={`new-place-form$-${id}`}
-                    onInput={handleInput}
-                    value={foundPlace[id]}
-                    valid={isValid}
-                    id={id}
-                    type={element}
-                    label={label}
-                    validators={validators}
-                    errorText={errorText}
-                />
-            )}
-            <Button
-                type='submit'
-                disabled={!formState.isValid}
-            >
-                Update Place
+      <>
+        <ErrorModal error={error} onClear={clearError} />
+        {!isLoading && loadedPlace && (
+          <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+            <Input
+              id="title"
+              element="input"
+              type="text"
+              label="Title"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Please enter a valid title."
+              onInput={inputHandler}
+              value={loadedPlace.title}
+              valid={true}
+            />
+            <Input
+              id="description"
+              element="textarea"
+              label="Description"
+              validators={[VALIDATOR_MINLENGTH(5)]}
+              errorText="Please enter a valid description (min. 5 characters)."
+              onInput={inputHandler}
+              value={loadedPlace.description}
+              valid={true}
+            />
+            <Button type="submit" disabled={!formData.isValid}>
+              Update Place
             </Button>
-        </form>
-    )
-}
-
-export default UpdatePlace;
+          </form>
+        )}
+      </>
+    );
+  };
+  
+  export default UpdatePlace;
